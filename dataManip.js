85d0d712930200f52c9ec30e15476c334b12d107
Tyler Crosse <tylerscottcrosse@gmail.com>
2017-03-26 11:15:42 -0700

    WIP async combine

--- dataManip.js
+++ dataManip.js
@@ -1,48 +1,66 @@
-const fs = require('fs');
+const fs       = require('fs');
+const readFile = require('fs-readfile-promise');
 
 const sourceDir = './redbkmon'
 
 fs.readdir(sourceDir, (err, files) => {
-  files.forEach(file => {
-    const readStream = fs.readFile(sourceDir + '/' + file, 'utf-8', (err, contents) => {
-      if (err) throw err;
+  // let colTitles;
+  // let body;
 
-      const csv = contents.split('\n');
-      const rawMeta = csv.slice(0, 8);
-      const head = csv.slice(9, 25);
+  files.forEach((file) => {
+    readFile(sourceDir + '/' + file, 'utf-8')
+      .then((contents) => {
+        const csv = contents.split('\n');
+        const rawMeta = csv.slice(0, 8);
+        const head = csv.slice(9, 25);
 
-      // strip and clean meta keys & vals
-      const metaKeys = [];
-      const metaVals = [];
-      for (let i=0; i<rawMeta.length; i++) {
-        const cleanMeta = rawMeta[i].replace(/[":]/g, '').trim().split(',');
-        metaKeys.push(cleanMeta[0]);
-        metaVals.push(cleanMeta[1]);
-      }
+        // strip and clean meta keys & vals
+        const metaKeys = [];
+        const metaVals = [];
+        for (let i=0; i<rawMeta.length; i++) {
+          const cleanMeta = rawMeta[i].replace(/[":]/g, '').trim().split(',');
+          metaKeys.push(cleanMeta[0]);
+          metaVals.push(cleanMeta[1]);
+        }
 
-      // clean column titles
-      let colTitles = [];
-      for (let i=0; i<head.length; i++) {
-        const colTitle = head[i].slice(11).slice(0, -1);
-        colTitles.push(colTitle.replace(/[,]/g, ''));
-      }
+        // clean column titles
+        let orginalColTitles = [];
+        for (let i=0; i<head.length; i++) {
+          const colTitle = head[i].slice(11).slice(0, -1);
+          orginalColTitles.push(colTitle.replace(/[,]/g, ''));
+        }
 
-      // build body
-      const body = csv.slice(26)
-        .map((row) => (
-          // join meta vals with orginal row data
-          metaVals.join(',') + ',' + row + '\n'
-        ))
-        .join('');
+        // build body portion from each file
+        const fileBody = csv.slice(26)
+          .map((row) => (
+            // join meta vals with orginal row data
+            metaVals.join(',') + ',' + row + '\n'
+          ))
+          .join('');
 
-      // build final doc
-      const doc = metaKeys.join(',') + ',' + colTitles.join(',') + '\n' + body;
-
-      const targetFile = './data/' + file.slice(0, -3) + 'csv';
-      fs.writeFile(targetFile, doc, (err) => {
-        if (err) throw err;
-        console.log('It saved! 🎊😀');
+        // build final doc
+        const colTitles = metaKeys.join(',') + ',' + orginalColTitles.join(',') + '\n';
+        // body += fileBody;
+        return {
+          colTitles,
+          fileBody
+        }
       })
-    });
-  })
+      .then((obj) => {
+        const { colTitles, fileBody } = obj;
+        console.log(colTitles)
+      })
+      .catch((err) => {
+        if (err) throw err;
+      });
+  });
+
+  // console.log('🍕', colTitles)
+  // const doc = colTitles + body;
+
+  // const targetFile = 'data.csv';
+  // fs.writeFile(targetFile, doc, (err) => {
+  //   if (err) throw err;
+  //   console.log(`${targetFile} saved! 🎊😀`);
+  // })
 });
